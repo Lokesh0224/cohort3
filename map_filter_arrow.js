4e580fd129e6553c87161bbd5b8caf4a4856b00a
Lokesh0224 <lokeshk20019@gmail.com>
2025-01-11 08:58:57 +0530

    updated filter, map, arrow

--- map_filter_arrow.js
+++ map_filter_arrow.js
@@ -43,16 +43,16 @@
 //*****************
 //filter function
 //Q)only even values, print true or false
-const arr= [1,2,3,4,5]
-const ans = arr.filter((n)=>{
-    if(n%2==0){
-        return true
-    }
-    else{
-        return false
-    }
-})
-console.log(ans)
+// const arr= [1,2,3,4,5]
+// const ans = arr.filter((n)=>{
+//     if(n%2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// })
+// console.log(ans)
 
 
 
